0e1cf880c00f56b67e460d787bea8798b2d150a9
Abdullah Anwar <abdullah860259@gmail.com>
2025-08-15 07:41:15 +0500

    asd

--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -6,7 +6,7 @@ export const config = { api: { bodyParser: false } };
 
 export async function POST(req) {
   const sig = req.headers.get("stripe-signature");
-  const buf = Buffer.from(await req.arrayBuffer()); // <-- fix for App Router
+  const buf = Buffer.from(await req.arrayBuffer(),'utf8'); // <-- fix for App Router
   let event;
 
   try {
