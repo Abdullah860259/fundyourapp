709181f27781b19bccfc7c9eca50623c605586e2
Hafiz Abdullah Anwer <169032711+Abdullah860259@users.noreply.github.com>
2025-08-15 06:21:38 +0500

    Update page.js

--- app/settings/page.js
+++ app/settings/page.js
@@ -8,7 +8,7 @@ const Page = () => {
   const [IsStripeConnected, setIsStripeConnected] = useState();
   const { data: session, status } = useSession();
   const clientId = process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID;
-  const redirectUri = encodeURIComponent('https://fundyourproject.vercel.app/api/stripe/oauth/callback');
+  const redirectUri = encodeURIComponent(`${process.env.NEXTAUTH_URL}/api/stripe/oauth/callback`);
   const connectUrl = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${clientId}&scope=read_write&redirect_uri=${redirectUri}`;
 
   useEffect(() => {
